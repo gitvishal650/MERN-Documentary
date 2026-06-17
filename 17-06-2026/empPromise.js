7f2fbbee3f48d4eef94adc1f21bc00f3a1ac9a0a
Vishal <vishal.panchalz2652@gmail.com>
2026-06-17 11:18:40 +0530

    promise

--- 17-06-2026/empPromise.js
+++ 17-06-2026/empPromise.js
@@ -3,52 +3,55 @@ let employees = [
     name: "vishal",
     age: 22,
     salary: 51000,
-    mobileno: 999999999
+    mobileno: 999999999,
   },
   {
     name: "lalit",
     age: 23,
     salary: 74000,
-    mobileno: 999999999
+    mobileno: 999999999,
   },
   {
     name: "rohan",
     age: 21,
     salary: 45000,
-    mobileno: 999999999
+    mobileno: 999999999,
   },
   {
     name: "navdeep",
     age: 26,
     salary: 72000,
-    mobileno: 999999999
+    mobileno: 999999999,
   },
   {
     name: "navdeep",
     age: 26,
     salary: 72000,
-    mobileno: 999999999
-  }
+    mobileno: 999999999,
+  },
 ];
 let newEmployee = {
   name: "Vineet",
   age: 24,
   salary: 90000,
-  mobileno: 999999999
+  mobileno: 999999999,
 };
 
 // problem statement
 // we will make two function, addEmployee() which will 3 second
 // another funtion displayEmployee() which will take zero second
 
-function addEmployee(newEmployee){
-    let pr = new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            console.log("Adding Employee...");
-            employees.push(newEmployee)
-            console.log("New Employee added successfully");
-            resolve(employees)
-        },3000)
-    })
+function addEmployee(newEmployee) {
+  let pr = new Promise((resolve, reject) => {
+      console.log("Adding Employee...");
+    setTimeout(() => {
+      employees.push(newEmployee);
+      console.log("New Employee added successfully");
+      resolve(employees);
+    }, 3000);
+  });
+  return pr;
 }
-addEmployee(newEmployee).then().catch();
+addEmployee(newEmployee)
+  .then((result) => console.log(result))
+  .catch();
