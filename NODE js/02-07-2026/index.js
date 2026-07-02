6863208f342fc67b6055b519c814794c6db52676
Vishal <vishal.panchalz2652@gmail.com>
2026-07-02 12:03:15 +0530

    staring express

--- NODE js/02-07-2026/index.js
+++ NODE js/02-07-2026/index.js
@@ -1,8 +1,12 @@
 // const math = require("./math.js");
 const hh = require("./assignment.js");
+const vol = require("../volumes/volumes.js")
 // console.log(math.add(2, 10));
 // console.log(math.mul(2, 10));
 
 console.log("Area of Right Angle Triangle :",hh.Rtriangle(2,5));
 console.log("Area of Rectangle :",hh.rectangle(2,5));
 console.log("Area of Circle :",hh.circle(5));
+
+console.log(vol.volume_cone(3,6));
+console.log(vol.volume_cube(2));
